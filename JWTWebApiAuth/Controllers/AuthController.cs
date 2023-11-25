0e91bfb7154f2b08b3f59c74bdde1d43ecbe458c
artyom <artyom.aghekyan2006@gmail.com>
2023-11-25 14:18:56 +0400

    Add Auth controller

---
+++ JWTWebApiAuth/Controllers/AuthController.cs
@@ -0,0 +1,75 @@
+﻿using BCrypt.Net;
+using JWTWebApiAuth.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JWTWebApiAuth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        public AuthController(IConfiguration config)
+        {
+            _config = config;
+        }
+        public static User user = new User();
+
+        [HttpPost("request")]
+        public ActionResult<User> Register(UserDto request)
+        {
+            string passwordHash = BCrypt.Net.BCrypt.HashPassword(request.Password);
+            user.Username = request.Username;
+            user.PasswordHash = passwordHash;
+            return Ok(user);
+
+
+        }
+        [HttpPost]
+        public ActionResult<User> Login(UserDto request)
+        {
+            if (request.Username != user.Username)
+            {
+                return NotFound();
+            }
+            if (!BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
+            {
+                return BadRequest("Wrong username or password");
+            }
+            string token = CreateToken(user);
+            return Ok(token);
+
+
+        }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes
+                (_config.GetSection("AppSettings:Token")
+                .Value!));
+            var creds = new SigningCredentials
+                (key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                    claims: claims,
+                    expires: DateTime.Now.AddDays(1),
+                    signingCredentials: creds
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+    }
+}
